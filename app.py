938483cc2eac252e88b7ec28acede9bb0262a7c1
Matthew Violet <mattviolet123@gmail.com>
2018-04-09 11:11:53 -0400

    beep

--- app.py
+++ app.py
@@ -25,11 +25,13 @@ def webhook():
 
   print(command)
   if (command == 'roast'):
+  	print("hey")
   	#ontents = urllib.request.urlopen("https://ngk7xk5ra5.execute-api.us-east-1.amazonaws.com/prod/get-insult?name=" + message[1].lower())
   	#msg = contents.replace('""', '')
   	#send_message()
   elif (command == 'addroast'):
   	roast = (" ".join(message[2:])
+  	print("roast")
   	#contents = urllib.request.urlopen("https://ngk7xk5ra5.execute-api.us-east-1.amazonaws.com/prod/add-insult?name=matt&insult=apptest")
 
 
