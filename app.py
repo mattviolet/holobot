d534b017298786000ddf355a616a7c23bf4aba09
Matthew Violet <mattviolet123@gmail.com>
2019-05-19 20:01:44 -0400

    adding holotbot to github

--- app.py
+++ app.py
@@ -12,19 +12,10 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-#insults = ['Levine smells like shit', 'Kelsey is just an idea', 'Levine is a pussy vegan bitch','Levine is an adderall filled crack head', 'Levine wishes he was ferda', 'If Levine was my FASET leader I would transfer the instant I heard that faggot\'s voice', 'Levine is a broke boy', 'Levine didn\'t make the middle school basketball team because he is a short jittery fuck', 'Levine is a kike', 'Levine is a blind four eyes weird looking tall grass head having fuck', 'Levine trashes room 1 because he is a disrespectful fuck and doesn\'t deserve to hang with the boys']
 
 @app.route('/', methods=['POST'])
 def webhook():
   data = request.get_json()
-
-  # if (data['text'] == 'add'):
-  #   print('reached here')
-  #   string = "appendtest space"
-  #   ##learn how to escape just spaces
-  #   link = urllib.parse.quote("https://ngk7xk5ra5.execute-api.us-east-1.amazonaws.com/prod/add-insult?name=matt&insult=" + string, safe='/:?=&')
-  #   contents = urllib.request.urlopen(link).read()
-
 
   ## either [roast, name] or [addRoast, name, insult]
   message = data['text'].split(' ')
