ddf86c1a81e2637331592c4868d7d227c97ec353
Matthew Violet <mattviolet123@gmail.com>
2021-05-22 18:55:21 -0400

    added logging statements to have a better idea of what is going on

--- app.py
+++ app.py
@@ -12,13 +12,10 @@ import requests
 
 app = Flask(__name__)
 
-
 @app.route('/', methods=['POST'])
 def webhook():
   data = request.get_json()
 
-  print(data)
-  print("i just receieved a post request")
   ## either [roast, name] or [addRoast, name, insult]
   message = data['text'].split(' ')
   ## either roast or addRoast
@@ -27,18 +24,27 @@ def webhook():
   if (len(message) > 2):
     roast = " ".join(message[2:]).replace(u"\2019", "'")
 
-  print(command)
+  print("this is the command: " + command)
+  print("")
   if ((command == 'roast') and (len(message) > 1)):
     payload = {'name': message[1].lower()}
-    msg = requests.get("https://nzj7ckgwkf.execute-api.us-east-2.amazonaws.com/Production/get-insult", params=payload)
-    print(msg)
-    #send_message(msg)
-  #elif ((command == 'addroast') and (len(message)> 2)):
-      #name = message[1].lower()
-      #print(name)
-      #print(roast)
+
+    #I can probably just set this up on the same mongodb database I used for cooking school?
+    #otherwise I might download mongo and try to dockerize it and get it running full time, not sure. Don't I need it to exist (be hosted) somewhere? Will have to see
+    ##msg = requests.get("https://nzj7ckgwkf.execute-api.us-east-2.amazonaws.com/Production/get-insult", params=payload)
+    roast = "danny lewis is a fraudulent"
+    send_message(roast)
+  elif ((command == 'addroast') and (len(message)> 2)):
+      name = message[1].lower()
+      print("This is what is about to be sent to add roast")
+      print("")
+
+      payload = {"name" : name, "roast" : roast}
+      print(payload)
+
       #link = urllib.parse.quote("https://ngk7xk5ra5.execute-api.us-east-1.amazonaws.com/prod/add-insult?name=" + name + "&insult=" + roast, safe='/:?=&')
-      #print(link)
+      
+      #I think this is the equivalent of doing a post request, not sure though.
       #contents = urllib.request.urlopen(link).read()
 
 
@@ -46,11 +52,15 @@ def webhook():
   return "ok", 200
 
 def send_message(msg):
-  url  = 'https://api.groupme.com/v3/bots/post'
+  #url  = 'https://api.groupme.com/v3/bots/post'
+  url = 'http://localhost:5000/'
 
   data = { 
           'bot_id' : '2e5e052a7c5b46862969084b43', #I want to use a heroku config variable for this to follow best practices :)
           'text'   : msg,
          }
-  request = Request(url, urlencode(data).encode())
-  json = urlopen(request).read().decode()
+  print("The data you are about to send to the server")
+  print(data)
+
+  ##request = Request(url, urlencode(data).encode())
+  ##json = urlopen(request).read().decode()
