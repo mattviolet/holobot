71df3e63fd62016421565ea4a72aa8e946c028e7
Matthew Violet <mattviolet123@gmail.com>
2021-05-25 19:02:12 -0700

    added many more changes, pretty much working

--- app.py
+++ app.py
@@ -3,7 +3,9 @@ import random
 import os
 import json
 import urllib.request
+import pymongo
 
+from pprint import pprint
 from urllib.parse import urlencode, quote
 from urllib.request import Request, urlopen
 
@@ -12,55 +14,64 @@ import requests
 
 app = Flask(__name__)
 
+client = pymongo.MongoClient("mongodb+srv://mviolet:Mlab4016449821@holotbot.stvmv.mongodb.net/holobot?retryWrites=true&w=majority")
+db = client["holobot"]
+names = db["names"]
+
 @app.route('/', methods=['POST'])
 def webhook():
+  print("")
   data = request.get_json()
-
+  print(data)
   ## either [roast, name] or [addRoast, name, insult]
-  message = request.get_json()['text'].split(' ')
+  message = data['text'].split(' ')
+  name = message[1]
   ## either roast or addRoast
   command = message[0].lower()
+  target = names.find_one({"name" : name })
+  print(target)
 
-  if (len(message) > 2):
-    roast = " ".join(message[2:]).replace(u"\2019", "'")
 
-  print("this is the command: " + command)
+  print("this is the message: " + data['text'])
   print("")
-  if ((command == 'roast') and (len(message) > 1)):
-    payload = {'name': message[1].lower()}
-
-    #I can probably just set this up on the same mongodb database I used for cooking school?
-    #otherwise I might download mongo and try to dockerize it and get it running full time, not sure. Don't I need it to exist (be hosted) somewhere? Will have to see
-    ##msg = requests.get("https://nzj7ckgwkf.execute-api.us-east-2.amazonaws.com/Production/get-insult", params=payload)
-    roast = "danny lewis is a fraudulent"
-    send_message(roast)
+  if ((command == 'roast') and (len(message) > 1)): 
+    roast = random.choice(target['insults'])
+    send_roast(roast)
   elif ((command == 'addroast') and (len(message)> 2)):
-      name = message[1].lower()
-      print("This is what is about to be sent to add roast")
-      print("")
+      ##get the name and roast
+      roast = " ".join(message[2:]).replace(u"\2019", "'")
 
-      payload = {"name" : name, "roast" : roast}
-      print(payload)
+      #get the current list of roasts for this person
+      current_insults = target['insults']
+      #add the new roast
+      current_insults.append(roast)
+      new_insults = current_insults
 
-      #link = urllib.parse.quote("https://ngk7xk5ra5.execute-api.us-east-1.amazonaws.com/prod/add-insult?name=" + name + "&insult=" + roast, safe='/:?=&')
-      
-      #I think this is the equivalent of doing a post request, not sure though.
-      #contents = urllib.request.urlopen(link).read()
+      #update the list in the db to the new list with the new roast
+      names.update_one({"_id" : target["_id"]},
+      {'$set':
+        { 'insults' : new_insults }  
+      })
 
+      for target in names.find():
+        print("new object: ")
+        print(target)
 
   # We don't want to reply to ourselves
   return "ok", 200
 
-def send_message(msg):
-  #url  = 'https://api.groupme.com/v3/bots/post'
-  url = 'http://localhost:5000/'
+def send_roast(roast):
+  groupme  = 'https://api.groupme.com/v3/bots/post'
+  #url = 'http://localhost:5000/'
 
-  data = { 
+  payload = { 
           'bot_id' : '2e5e052a7c5b46862969084b43', #I want to use a heroku config variable for this to follow best practices :)
-          'text'   : msg,
+          'text'   : roast,
          }
-  print("The data you are about to send to the server")
-  print(data)
 
-  ##request = Request(url, urlencode(data).encode())
-  ##json = urlopen(request).read().decode()
+  print("The data you are about to send to the server")
+  print(payload)
+
+  reqString = json.dumps(payload)
+  request = requests.post(groupme, data = reqString)
+
